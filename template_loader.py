07fc7c59b66de33b5c5a773063e03814151e8e75
Kevin Miller <kmiller@w99x788i.waxie.com>
2019-01-15 16:00:20 -0800

    this is a test

--- template_loader.py
+++
@@ -1,7 +0,0 @@
-from jinja2 import Environment, PackageLoader, select_autoescape
-env = Environment(
-    loader=PackageLoader('yourapplication', 'templates'),
-    autoescape=select_autoescape(['html', 'xml'])
-    )
-
-
