4e76092db84040de1a808628fb6caf60d538e03c
Kevin Miller <kmiller@w99x788i.waxie.com>
2019-01-23 10:50:57 -0800

    remove render engine

--- render_engine/summaries.py
+++
@@ -1,6 +0,0 @@
-def latest_blog(latest_post)
-    if 'summary' not in latest_post:
-        start_index = 120
-        while latest_post['content'][start_index] not in string.punctuation:
-            start_index += 1
-            latest_post['summary'] = latest_post['content'][:start_index + 1] + '...'
