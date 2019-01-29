16a6a445b339cf4e94e55ede0b938a1a4a5d429d
Kevin Miller <kmiller@w99x788i.waxie.com>
2019-01-29 09:59:16 -0800

    move newsletter into container

--- pages/microblog.py
+++ pages/microblog.py
@@ -5,4 +5,24 @@ class MicroBlogPost(BlogPost):
     title = ''
     def __init__(self, base_file):
         super().__init__(base_file)
+    
+    
+    def get_date_published(self):
+        """Returns the value of _date_published or _date, or created_datetime from
+the system if not defined. NOTE THE SYSTEM DATE IS KNOWN TO CAUSE
+ISSUES WITH FILES THAT WERE COPIED OR TRANSFERRED WITHOUT THEIR
+METADATA BEING TRANSFER READ AS WELL"""
 
+        if self._date_published:
+            date = arrow.get(self._date_published, config.TIME_FORMAT)
+
+        elif self._date:
+            date = arrow.get(self._date, config.TIME_FORMAT)
+
+        elif self.base_file:
+            if arrow.get(self.base_file.stem, 'YYYYMMDDHHmm')
+
+        else:
+             date = get_ct_time(self.base_file)
+
+        return date.format(config.TIME_FORMAT)
