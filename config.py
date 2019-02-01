8bf866413dd06d33ddb443233f53f2f6027d8af1
Jay Miller <kjaymiller@Jays-MacBook-Pro.local>
2019-01-31 19:59:10 -0800

    images

--- config.py
+++ config.py
@@ -30,8 +30,9 @@ HEADER_LINKS = (
     Link(name='Newsletter', url='/subscribe.html'),
     Link('Contact','/contact.html'),
     Link('PIT', 'https://productivityintech.com'),
-    Link('Podcasts', links=[Link('Productivity in Tech Podcast', 'https://productivityintech.transistor.fm'),
-                           Link('Ask A Brit', 'https://askabrit.transistor.fm'),
-                           Link('Dev Otaku', 'https://devotaku.transistor.fm')
+    Link('Podcasts', links=[
+        Link('Productivity in Tech Podcast', 'https://productivityintech.transistor.fm', image="https://s3-us-west-2.amazonaws.com/kjaymiller/images/pit-podcast.png"),
+                            Link('Ask A Brit', 'https://askabrit.transistor.fm'),
+                            Link('Dev Otaku', 'https://devotaku.transistor.fm')
                            ])
 )
