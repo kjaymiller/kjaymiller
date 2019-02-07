97ebf891e2c2094deef3faa44155879b8ba5d00a
Kevin Miller <kmiller@w99x788i.waxie.com>
2019-02-06 16:42:36 -0800

    fix RFC3339

--- pages/collection.py
+++ pages/collection.py
@@ -8,7 +8,7 @@ import arrow
 
 def rfc3339(time):
     rfc_time = arrow.get(time, config.TIME_FORMAT).format('YYYY-MM-DDTHH:MM:SSZ')
-    return rfc_time + 'Z'
+    return rfc_time 
      
     return 
 class Collection:
