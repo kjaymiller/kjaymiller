53303742966f96073323fff0ae7581dec3d778d4
Kevin Miller <kmiller@w99x788i.waxie.com>
2019-02-08 11:57:59 -0800

    new post

--- run.py
+++ run.py
@@ -59,8 +59,9 @@ def index():
             })
 
 
-    latest_posts = sorted(blog.pages, key=lambda page: page.date_published)
-    latest_microposts = sorted(microblog.pages, key=lambda page: page.date_published)
+    latest_posts = sorted(blog.pages, key=lambda page: page.date_published)[:3]
+    latest_microposts = sorted(microblog.pages, 
+            key=lambda page: page.date_published)[:3]
     return Page(template='index.html', 
             what_im_block=what_im_block, 
             featured_post=latest_posts[0],
