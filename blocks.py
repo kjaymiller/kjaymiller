08e1d4107ce24c853b6bb1b413c3ad4cc376d433
Kevin Miller <kmiller@w99x788i.waxie.com>
2019-03-18 10:22:40 -0700

    updating information

--- blocks.py
+++ blocks.py
@@ -1,32 +1,41 @@
 what_im_block = ({
-    'drinking': [
-        {
-            'title': 'Intelligentsia - Frequency Blend',
-            'url': 'https://www.intelligentsiacoffee.com/frequency-blend',
-            },
-        ],
-    'playing': [
-        {
-            'title': "Hearthstone", 
-            'url':'https://playhearthstone.com/en-us/', 
-            },
-        {
-            'title': 'Tetris99',
-            'url': 'https://www.nintendo.com/games/detail/tetris-99-switch',
-            },
-        {
-            'title': 'Puyo Puyo Tertris',
-            'url': 'https://amzn.to/2RTv4oP',
-            },
-        {
-            'title': 'Tetris Effect',
-            'url': 'https://amzn.to/2Wo4BOX',
-            }
-        ], 
-    'reading': [
-        {
-            'title': 'One Small Step Can Change Your Life',
-            'url': 'https://amzn.to/2XTWvyv',
-            },
-        ],
-    })
+    'drinking': {
+        'icon': 'fas fa-mug-hot', 
+        'items': [
+            {
+                'title': 'Intelligentsia - Frequency Blend',
+                'url': 'https://www.intelligentsiacoffee.com/frequency-blend',
+                },
+            ],
+        },
+
+    'playing': {
+        'icon': 'fas fa-gamepad',
+        'items': [
+            {
+                'title': "Hearthstone", 
+                'url':'https://playhearthstone.com/en-us/', 
+                },
+            {
+                'title': 'Tetris99',
+                'url': 'https://www.nintendo.com/games/detail/tetris-99-switch',
+                },
+            {
+                'title': 'Puyo Puyo Tertris',
+                'url': 'https://amzn.to/2RTv4oP',
+                },
+            {
+                'title': 'Tetris Effect',
+                'url': 'https://amzn.to/2Wo4BOX',
+                }
+            ], 
+
+        'reading': 
+                'icon': 'fas fa-book'
+                'items': [
+            {   
+                'title': 'One Small Step Can Change Your Life',
+                'url': 'https://amzn.to/2XTWvyv',
+                },
+            ],
+        })
