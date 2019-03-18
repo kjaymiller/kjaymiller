2f5b5c8c35335ed763f22750ca6cbeff817f6cca
Kevin Miller <kmiller@w99x788i.waxie.com>
2019-03-18 10:30:23 -0700

    Update syntax

--- blocks.py
+++ blocks.py
@@ -1,37 +1,37 @@
 what_im_block = {
-    'drinking': {
-        'icon': 'fas fa-mug-hot', 
-        'items': [
-            {
-                'title': 'Intelligentsia - Frequency Blend',
-                'url': 'https://www.intelligentsiacoffee.com/frequency-blend',
-                },
-            ],
-        },
+        'Drinking': {
+            'icon': 'fas fa-mug-hot', 
+            'items': [
+                {
+                    'title': 'Intelligentsia - Frequency Blend',
+                    'url': 'https://www.intelligentsiacoffee.com/frequency-blend',
+                    },
+                ],
+            },
 
-    'playing': {
-        'icon': 'fas fa-gamepad',
-        'items': [
-            {
-                'title': "Hearthstone", 
-                'url':'https://playhearthstone.com/en-us/', 
-                },
-            {
-                'title': 'Tetris99',
-                'url': 'https://www.nintendo.com/games/detail/tetris-99-switch',
-                },
-            {
-                'title': 'Puyo Puyo Tertris',
-                'url': 'https://amzn.to/2RTv4oP',
-                },
-            {
-                'title': 'Tetris Effect',
-                'url': 'https://amzn.to/2Wo4BOX',
-                }
-            ],
-        },
+        'Playing': {
+            'icon': 'fas fa-gamepad',
+            'items': [
+                {
+                    'title': "Hearthstone", 
+                    'url':'https://playhearthstone.com/en-us/', 
+                    },
+                {
+                    'title': 'Tetris99',
+                    'url': 'https://www.nintendo.com/games/detail/tetris-99-switch',
+                    },
+                {
+                    'title': 'Puyo Puyo Tertris',
+                    'url': 'https://amzn.to/2RTv4oP',
+                    },
+                {
+                    'title': 'Tetris Effect',
+                    'url': 'https://amzn.to/2Wo4BOX',
+                    }
+                ],
+            },
 
-        'reading': {
+        'Reading': {
             'icon': 'fas fa-book',
             'items': [
                 {   
