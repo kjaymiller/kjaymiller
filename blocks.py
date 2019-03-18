4b81b90923a030649d3115dca00f9fbb5d35f764
Kevin Miller <kmiller@w99x788i.waxie.com>
2019-03-18 08:55:28 -0700

    What im block

---
+++ blocks.py
@@ -0,0 +1,32 @@
+what_im_block = ({
+    'drinking': [
+        {
+            'title': 'Intelligentsia - Frequency Blend',
+            'url': 'https://www.intelligentsiacoffee.com/frequency-blend',
+            },
+        ],
+    'playing': [
+        {
+            'title': "Hearthstone", 
+            'url':'https://playhearthstone.com/en-us/', 
+            },
+        {
+            'title': 'Tetris99',
+            'url': 'https://www.nintendo.com/games/detail/tetris-99-switch',
+            },
+        {
+            'title': 'Puyo Puyo Tertris',
+            'url': 'https://amzn.to/2RTv4oP',
+            },
+        {
+            'title': 'Tetris Effect',
+            'url': 'https://amzn.to/2Wo4BOX',
+            }
+        ], 
+    'reading': [
+        {
+            'title': 'One Small Step Can Change Your Life',
+            'url': 'https://amzn.to/2XTWvyv',
+            },
+        ],
+    })
--- run.py
+++ run.py
@@ -11,6 +11,7 @@ from pages import (
         )
 from pages.generators import gen_static 
 from pages.writer import write_page, writer
+from blocks import what_im_block
 
 
 pages = Collection(name='pages', content_type=Page, content_path='pages')
@@ -30,40 +31,6 @@ for collection in page_collections:
 
 @writer(route='index.html')
 def index():
-    what_im_block = ({
-        'drinking': [
-            {
-                'title': 'Intelligentsia - Frequency Blend',
-                'url': 'https://www.intelligentsiacoffee.com/frequency-blend',
-                },
-                ],
-        'playing': [
-            {
-                'title': "Hearthstone", 
-                'url':'https://playhearthstone.com/en-us/', 
-                },
-            {
-                'title': 'Tetris99',
-                'url': 'https://www.nintendo.com/games/detail/tetris-99-switch',
-                },
-            {
-                'title': 'Puyo Puyo Tertris',
-                'url': 'https://amzn.to/2RTv4oP',
-                },
-            {
-                'title': 'Tetris Effect',
-                'url': 'https://amzn.to/2Wo4BOX',
-                }
-            ], 
-        'reading': [
-            {
-                'title': 'One Small Step Can Change Your Life',
-                'url': 'https://amzn.to/2XTWvyv',
-                },
-            ],
-            })
-
-
     latest_posts = blog.pages[:4]
     latest_microposts = microblog.pages[:3] 
             
