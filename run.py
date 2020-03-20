86514536068a1a0987f33fa66b60a22fa76eb6cb
kjaymiller <kjaymiller@gmail.com>
2020-03-20 07:53:19 -0700

    subposts

--- run.py
+++ run.py
@@ -1,29 +1,30 @@
 import logging
-import pendulum
 from render_engine import Site, Page, Collection
-from render_engine.microblog import MicroBlog
 from render_engine.blog import Blog
+from render_engine.microblog import MicroBlog
 from render_engine.links import Link
+
+# logging.basicConfig(level=logging.INFO)
 
 HEADER_LINKS = (
     Link(name='Blog', url='/all_posts.html'),
-    Link(name='MicroBlog', url='/all_microblog_posts.html'),
-    Link(name='Talks', url='/conference-talks.html'),
-    Link(name='Newsletter', url='https://buttondown.email/productivityintech'),
+    Link(name='MicroBlog', url='https://micro.blog/kjaymiller'),
+    Link(name='Newsletter', url='/newsletter'),
     Link(name='Productivity in Tech', url='https://productivityintech.com'),
     Link(name='Contact', url='/contact'),
-    Link(name='Podcasts', url='/podcasts.html'),
-    Link(name='GitHub', url='https://github.com/kjaymiller', icon="fab fa-github"),
-    Link(name='YouTube', url='https://www.youtube.com/channel/UCjoJU65IbXkKXsNqydro05Q', icon="fab fa-youtube"),
-    Link(name='Twitter', url='https://twitter.com/kjaymiller', icon="fab fa-twitter"),
-    Link(name='Micro.Blog', url='https://micro.blog/kjaymiller', icon="fab fa-microblog"),
-    Link(name='Resume', url='/static/files/Jay_Miller_-_Software_Engineer.pdf'),
+    Link(name='YouTube', url='https://www.youtube.com/channel/UCjoJU65IbXkKXsNqydro05Q'),
     )
 
 mysite = Site(strict=True)
-mysite.SITE_TITLE = '(K)Jay Miller'
-mysite.SITE_LINK = 'https://kjaymiller.com'
+mysite.SITE_TITLE = 'Jay Miller'
+mysite.SITE_URL = 'https://kjaymiller.com'
 mysite.HEADER_LINKS = HEADER_LINKS
+
+@mysite.register_route
+class Index(Page):
+    template = "index.html"
+    slug = "index.html"
+
 
 @mysite.register_collection
 class Pages(Collection):
@@ -31,45 +32,18 @@ class Pages(Collection):
     content_path = "content/pages"
     template = "page.html"
 
+
 @mysite.register_collection
 class Blog(Blog):
     routes = ['', '/blog']
-    template = "blog.html"
-
-
-@mysite.register_collection
-class Talks(Collection):
-    routes = ['', '/talks']
-    content_path = 'content/talks'
-    template = "blog.html"
-
+    template = 'archive.html'
+    subcollections = ['category', 'tags']
 
 @mysite.register_collection
 class MicroBlog(MicroBlog):
     routes = ['/microblog']
-    template = "blog.html"
-    content_path = "content/microblog"
-
-@mysite.register_route
-class Index(Page):
-    template = "index.html"
-    slug = "index.html"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.posts = list(
-                sorted(
-                    mysite.collections['Blog'].pages,
-                    key=lambda x:x.date,
-                    reverse=True)
-            )[:4]
-
-        self.microblog_posts = list(
-                sorted(
-                    mysite.collections['MicroBlog'].pages,
-                    key=lambda x:x.date,
-                    reverse=True)
-                )[:5]
+    template = 'archive.html'
 
 
+print([x.slug for x in mysite.routes])
 mysite.render(dry_run=False)
