8f68003c0191da8095cfb8bc094c2650a8de9775
kjaymiller <kjaymiller@gmail.com>
2020-07-10 16:04:36 -0700

    new blog post

--- .github/actions/update_readme.py
+++ .github/actions/update_readme.py
@@ -2,8 +2,6 @@ import feedparser
 from pathlib import Path
 from jinja2 import Template
 
-rss_feed = './output/blog.rss.xml'
-podcast_url = 'https://feeds.transistor.fm/productivity-in-tech-podcast'
 
 def get_latest_post(rss_feed):
     f = feedparser.parse(rss_feed)
@@ -14,10 +12,13 @@ def get_latest_post(rss_feed):
             }
 
 # update readme
-template = Template(Path('./README_template.md').read_text())
-Path('./README.md').write_text(
-        template.render(
-            latest_post=get_latest_post(rss_feed),
-            latest_podcast_post=get_latest_post(podcast_url),
+if __name__ == '__main__':
+    rss_feed = './output/blog.rss.xml'
+    podcast_url = 'https://feeds.transistor.fm/productivity-in-tech-podcast'
+    template = Template(Path('./README_template.md').read_text())
+    Path('./README.md').write_text(
+            template.render(
+                latest_post=get_latest_post(rss_feed),
+                latest_podcast_post=get_latest_post(podcast_url),
+                )
             )
-        )
