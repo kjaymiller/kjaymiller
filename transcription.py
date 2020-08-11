cbcba243104750bd1d6c2c69ab33a8e6e7ebace3
kjaymiller <kjaymiller@gmail.com>
2020-08-11 15:33:50 -0700

    add transcription.py

---
+++ transcription.py
@@ -0,0 +1,24 @@
+import typer
+from pathlib import Path
+from transcriptor.amazon import AmazonJob
+
+app = typer.Typer()
+
+@app.command()
+def upload_file(filepath: str):
+    job = AmazonJob(filepath=filepath, bucket='pit-transcriptions')
+    job.start()
+
+@app.command()
+def build_file(filepath: str, output_file: Path):
+    job = AmazonJob(filepath=filepath, bucket='pit-transcriptions', status='foo')
+
+    if job.status == 'COMPLETED':
+        job.build()
+        output_file.write_text(job.transcript)
+
+    else: 
+        typer.echo(job.status)
+
+if __name__ == "__main__":
+    app()
