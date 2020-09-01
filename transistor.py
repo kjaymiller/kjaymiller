2b8ebb0c0f94207c9a00dfd4069d841e6456dac5
Jay Miller <kjaymiller@jays-mbp-2.lan>
2020-09-01 14:00:47 -0700

    add docstrings

--- transistor.py
+++ transistor.py
@@ -8,12 +8,16 @@ app = typer.Typer()
 
 @app.command()
 def get_show_list():
+    """Get a list of all the shows"""
+
     url = 'https://api.transistor.fm/v1/shows'
     r = httpx.get(url, headers=header)
     typer.echo([(x['id'], x['attributes']['title']) for x in r.json()['data']])
 
 @app.command()
 def get_latest_episode( output: Path, show_id: int=799):
+    """Fetch the Latest Episode and write a render-engine template to the output"""
+
     episodes_url = 'https://api.transistor.fm/v1/episodes/'
     params = {"show_id": show_id}
     r = httpx.get(episodes_url, headers=header, params=params)
@@ -24,10 +28,14 @@ def get_latest_episode( output: Path, show_id: int=799):
     published_date = episode_attrs['published_at']
     summary = episode_attrs['summary']
     embed_url = episode_attrs['embed_html']
+
     content = f"""title: {title}
-    date: {published_date}
-    {summary}
-    {embed_url}"""
+date: {published_date}
+images: https://kjaymiller.s3-us-west-2.amazonaws.com/images/pit-logo-v5.jpg
+
+{summary}
+{embed_url}"""
+
     output.write_text(content)
 
 
