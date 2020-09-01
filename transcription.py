c2eadf8c3444810d21e3c18cd77c0e38720a7e8a
Jay Miller <kjaymiller@jays-mbp-2.lan>
2020-09-01 12:57:07 -0700

    Revert "update add transistor api"
    This reverts commit 5f4bfcf25964cafef03e618380c406ce3c26504c.

--- transcription.py
+++ transcription.py
@@ -4,33 +4,21 @@ from transcriptor.amazon import AmazonJob
 
 app = typer.Typer()
 
-
 @app.command()
-def upload_file(filepath: str, speakers: int=0):
+def upload_file(filepath: str):
     job = AmazonJob(filepath=filepath, bucket='pit-transcriptions')
-
-    if speakers > 1:
-        job.start(speakers=speakers)
-
-    else:
-        job.start()
+    job.start()
 
 @app.command()
-def build_file(filepath: str, output_file: Path, format:str='transcript'):
+def build_file(filepath: str, output_file: Path):
     job = AmazonJob(filepath=filepath, bucket='pit-transcriptions', status='foo')
 
     if job.status == 'COMPLETED':
         job.build()
-
-        if format == 'transcript':
-            output_file.write_text(job.transcript)
-
-        elif  format == 'srt':
-            output_file.write_text(job.srt)
+        output_file.write_text(job.transcript)
 
     else:
         typer.echo(job.status)
-
 
 if __name__ == "__main__":
     app()
