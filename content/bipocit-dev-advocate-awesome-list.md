ffb3b32a9a21be284c4d050c9b922fbb50d6bd31
Jay Miller <kjaymiller@Jays-MacBook-Pro-2.local>
2020-10-21 16:34:35 -0700

    update transistor post

--- content/bipocit-dev-advocate-awesome-list.md
+++ content/bipocit-dev-advocate-awesome-list.md
@@ -1,1 +1,6 @@
-title: BIPOCIT Awesome List Because Discovery is Hard
+title: BIPOCIT Dev Advocates Awesome List
+subtitle: Because Discovery is Hard
+date: 21 Oct 2020 16:33
+link: https://github.com/kjaymiller/awesome-bipocit-dev-advocates
+
+coming soon!
--- transistor.py
+++ transistor.py
@@ -1,7 +1,9 @@
 import os
 import httpx
 import typer
+import slugify
 from pathlib import Path
+
 
 header = {'x-api-key': os.environ.get('transistorKey')}
 app = typer.Typer()
@@ -15,27 +17,29 @@ def get_show_list():
     typer.echo([(x['id'], x['attributes']['title']) for x in r.json()['data']])
 
 @app.command()
-def get_latest_episode( output: Path, show_id: int=799):
+def get_latest_episode(directory: Path, show_id: int=799, episodes: int=1):
     """Fetch the Latest Episode and write a render-engine template to the output"""
 
     episodes_url = 'https://api.transistor.fm/v1/episodes/'
     params = {"show_id": show_id}
     r = httpx.get(episodes_url, headers=header, params=params)
-    episode = r.json()['data'][1]
-    episode_attrs = episode['attributes']
-    title = episode_attrs['title']
-    published_date = episode_attrs['published_at']
-    summary = episode_attrs['summary']
-    embed_url = episode_attrs['embed_html']
 
-    content = f"""title: {title}
+    for episode in r.json()['data'][0:episodes]:
+        episode_attrs = episode['attributes']
+        title = episode_attrs['title']
+        published_date = episode_attrs['published_at']
+        summary = episode_attrs['summary']
+        embed_url = episode_attrs['embed_html']
+
+        content = f"""title: {title}
 date: {published_date}
 images: https://kjaymiller.s3-us-west-2.amazonaws.com/images/pit-logo-v5.jpg
 
 {summary}
 {embed_url}"""
 
-    output.write_text(content)
+        directory.joinpath(slugify(episode['title'])).with_suffix(".md")
+        output.write_text(content)
 
 
 if __name__ == "__main__":
