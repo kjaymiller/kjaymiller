eac4534f6f8d0a6887a95e59dfebe1bd5415c327
Jay Miller <kjaymiller@Jays-MacBook-Pro-2.local>
2020-10-21 15:59:03 -0700

    update with tags

--- transcription.py
+++ transcription.py
@@ -1,24 +1,20 @@
 import typer
 from pathlib import Path
-from transcriptor.amazon import AmazonJob
+from transcriptor.amazon import AmazonEnv
 
 app = typer.Typer()
 
 @app.command()
-def upload_file(filepath: str):
-    job = AmazonJob(filepath=filepath, bucket='pit-transcriptions')
-    job.start()
+def upload_file(filepath: str, speakers:int=1, uploaded:bool=False):
+    job = AmazonEnv(audio_file=filepath, bucket='pit-transcriptions')
+    job.is_uploaded = uploaded
+    job.start_transcription(speaker_count=speakers)
 
 @app.command()
 def build_file(filepath: str, output_file: Path):
-    job = AmazonJob(filepath=filepath, bucket='pit-transcriptions', status='foo')
-
-    if job.status == 'COMPLETED':
-        job.build()
-        output_file.write_text(job.transcript)
-
-    else:
-        typer.echo(job.status)
+    job = AmazonEnv(audio_file=filepath, bucket='pit-transcriptions')
+    transcription = job.build()
+    output_file.write_text(transcription.to_text())
 
 if __name__ == "__main__":
     app()
