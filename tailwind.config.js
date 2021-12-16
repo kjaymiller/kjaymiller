3862a24ec7c76ea42f0b601557f859afd405daf8
Jay Miller <kjaymiller@gmail.com>
2021-12-16 13:52:49 -0800

    clean up css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,16 @@
 module.exports = {
-  purge: {
-    enabled: true,
-    mode: 'all',
+    mode: 'jit',
     content: [
       './output/**/*.html',
       './output/**/*.js',
     ],
-  },
   theme: {
     extend: {},
   },
   variants: {},
   plugins: [
-    // ...
     require('tailwindcss'),
     require('autoprefixer'),
-    // ...
   ],
 }
+
