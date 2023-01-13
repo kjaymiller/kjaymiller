b061f00e9bf872ded6f78b4535ae0bb2f6886ecc
Jay Miller <kjaymiller@gmail.com>
2023-01-12 21:46:33 -0800

    clean conflicts

--- update_readme.py
+++ update_readme.py
@@ -9,21 +9,20 @@ CONDUIT_FEED = "https://www.relay.fm/conduit/feed"
 PCN_FEED = "https://feeds.transistor.fm/python-community-podcast"
 BLOG_FEED = "https://kjaymiller.com/blog.rss"
 
-if __name__ == "__main__":
-    Path('./readme.md').write_text(template.render(
-    latest_conduit_episode = get_episodes(CONDUIT_FEED)[0]
-    latest_pcn_episode = get_episodes(PCN_FEED)[0]
-    latest_post = sorted(
-    feedparser.parse(BLOG_FEED).entries,
-    key=lambda x: x.published_parsed,
-    reverse=True,
-    )[0]
+latest_conduit_episode = get_episodes(CONDUIT_FEED)[0]
+latest_pcn_episode = get_episodes(PCN_FEED)[0]
+latest_post = sorted(
+        feedparser.parse(BLOG_FEED).entries,
+        key=lambda x: x.published_parsed,
+        reverse=True,
+)[0]
 
+if __name__ == "__main__":
     template = environment.get_template(".readme_template.md")
     Path("./readme.md").write_text(
-    template.render(
+        template.render(
             latest_conduit_episode=latest_conduit_episode,
             latest_pcn_episode=latest_pcn_episode,
             latest_blog_post=latest_post,
-    )
+        )
     )
