03e7bce578d58eb1a51049dbfa177717c2612847
Jay Miller <kjaymiller@gmail.com>
2023-01-12 21:39:42 -0800

    update rss feeds

--- update_readme.py
+++ update_readme.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     latest_conduit_episode = get_episodes('https://www.relay.fm/conduit/feed')[0]
     latest_pcn_episode = get_episodes("https://feeds.transistor.fm/python-community-podcast")[0]
     template = environment.get_template('.readme_template.md')    
-    latest_post = sorted(feedparser.parse('https://kjaymiller.com/jay-miller-blog.rss').entries, key= lambda x: x.published_parsed, reverse=True)[0]
+    latest_post = sorted(feedparser.parse('https://kjaymiller.com/blog.rss').entries, key= lambda x: x.published_parsed, reverse=True)[0]
     
     Path('./readme.md').write_text(template.render(
             latest_conduit_episode=latest_conduit_episode,
