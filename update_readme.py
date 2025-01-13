0420b336e2e1e73b84f0ec0397fbde867316ff3b
Jay Miller <kjaymiller@gmail.com>
2025-01-13 13:44:10 -0500

    update template removing PCN

--- update_readme.py
+++ update_readme.py
@@ -7,20 +7,18 @@ environment = Environment(loader=FileSystemLoader("."))
 
 MICROBLOG = "https://kjaymiller.com/microblog.rss"
 CONDUIT_FEED = "https://www.relay.fm/conduit/feed"
-PCN_FEED = "https://feeds.transistor.fm/python-community-podcast"
 BLOG_FEED = "https://kjaymiller.com/blog.rss"
 
 latest_conduit_episode = get_episodes(CONDUIT_FEED)[0]
-latest_pcn_episode = get_episodes(PCN_FEED)[0]
 latest_microblog_post = sorted(
-        feedparser.parse(MICROBLOG).entries,
-        key=lambda x: x.published_parsed,
-        reverse=True,
+    feedparser.parse(MICROBLOG).entries,
+    key=lambda x: x.published_parsed,
+    reverse=True,
 )[0]
 latest_post = sorted(
-        feedparser.parse(BLOG_FEED).entries,
-        key=lambda x: x.published_parsed,
-        reverse=True,
+    feedparser.parse(BLOG_FEED).entries,
+    key=lambda x: x.published_parsed,
+    reverse=True,
 )[0]
 
 if __name__ == "__main__":
@@ -29,7 +27,6 @@ if __name__ == "__main__":
         template.render(
             latest_microblog_post=latest_microblog_post,
             latest_conduit_episode=latest_conduit_episode,
-            latest_pcn_episode=latest_pcn_episode,
             latest_blog_post=latest_post,
         )
     )
